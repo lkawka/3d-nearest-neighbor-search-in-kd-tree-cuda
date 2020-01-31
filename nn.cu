e000737e09c4cef694394e6730a917695be213d6
lilcoffee <luk.kawka@gmail.com>
2020-01-31 15:30:21 +0100

    test

--- nn.cu
+++ nn.cu
@@ -43,7 +43,7 @@ int main() {
     int3 *results;
     eChk(cudaMallocManaged(&results, N_QUERIES * sizeof(int3)));
 
-    nearestNeighborGPU<<<1024, 32>>>(tree, TREE_SIZE, queries, results, N_QUERIES);
+    nearestNeighborGPU<<<2056, 32>>>(tree, TREE_SIZE, queries, results, N_QUERIES);
     eChk(cudaDeviceSynchronize());
     
     auto end = std::chrono::system_clock::now();
